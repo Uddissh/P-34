3b483fd8eac730956fb78338f9b52ac66ca91ef8
Uddissh <68012232+Uddissh@users.noreply.github.com>
2020-12-12 19:22:26 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,67 +1,51 @@
-//Create variables here
-var dogIMG, happyDogIMG;
+var dog,happyDog,dogImg;
 var database;
-var foodS, foodStock;
-var dog;
+var foodS,foodStock;
 
-function preload()
-{
-  //load images here
-  dogIMG = loadImage("images/Dog.png");
-  happyDogIMG = loadImage("images/happydog.png");
+function preload(){
+dogImg = loadImage("images/Dog.png");
+happyDog = loadImage("images/happydog.png");
 }
 
 function setup() {
-  createCanvas(500, 500);
-  
-  dog = createSprite(50, 100, 50, 50);
-  dog.addImage(dogIMG);
-
-  foodStock = database.ref("food");
-  foodStock.on("value", readStock(foodStock));
-  
+  database = firebase.database();
+	createCanvas(500, 500);
+var dog = createSprite(250,300,150,150);
+ dog.addImage(dogImg);
+ dog.scale = 0.15;
+  foodStock = database.ref('Food');
+    foodStock.on("value",readStock);
+    textSize(20);
 }
 
 
 function draw() {  
+background(46,139,87)
 
-  background(46, 139, 87);
-
-  imageMode(CENTER);
-  image(dogIMG, 400, 350, 50, 100);
-  imageMode(CENTER);
-  image(happyDogIMG, 600, 350, 50, 100);
-
-  if (keyWentDown(UP_ARROW)) {
-    writeStock(foodS);
-    foodStock = foodStock - 1;
-    dog.addImage(happyDogIMG)
-  }
-
+if(keyWentDown(UP_ARROW)){
+  writeStock(foodS);
+  dog.addImage(happyDog);
+}
   drawSprites();
-  //add styles here
-
-  textSize(15);
-  Fill("black");
-
+  fill(255,255,254);
+  stroke("black");
+  text("food remaning :"+foodS,170,200);
+  textSize(13);
+  text("note : press up_arrow key to feed milk to the dog",130,10,300,20);
 
 }
-
-function readStock(data) {
-
+function readStock(data){
   foodS = data.val();
-  
-}
-
-function writeStock(x) {
-  
-  if (x<=0) {
-    x = 0;
-  }else{
-    x = x - 1;
   }
   
-  database.ref('/').update({
-    food:x
-  })
-}
+  function writeStock(x){
+    if(x <= 0){
+      x = 0;
+    }
+    else{
+      x = x-1;
+    }
+      database.ref("/").update({
+        Food : x,
+      })
+    }
